b6d36279a674c06b0140d704383f3fda8bf85647
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-12-19 23:43:26 +0300

    All images init and visible,

--- app/Kenny.md
+++ app/Kenny.md
@@ -5,11 +5,12 @@
 - [x] activity_main.xml aç ve -> 3 satır, 3 sütun kenny ekle, toplam 9 tane kenny olacak.
 - [x] Constraint olmuş olan Grid layout küçült ve hepsi ortada hizalansın.
 - [x] En üste
-      - [x] Time textView ekle
+    - [x] Time textView ekle
 
 - [x] En alta
-      - [x]  Score textView ekle ve Tekrar Infer Constraint yap.
+    -  [x]  Score textView ekle ve Tekrar Infer Constraint yap.
 
+</br>
 
 ##  2. Scoru Artır
 - [x] activity_main.xml içinden onClick ver, Hangi Kenny'e tıklanırsa tıklansın, score artsın
@@ -17,11 +18,30 @@
 - [x] Time id ver -> timeText
 - [x] Score id ver -> scoreText
 
+</br>
 
 ## 3. Score için MainActivity.java içinde:
 - [x] timeText ve scoreText id'li TextView lar için:
-         - [x] Classın içinde TextView Global olarak tanımla.
-         - [x] onCreate() metodu içinde ise findViewById ile onları al ve init et.
-                - [x] Yani  1. -> Global Olarak Tanımla
-                - [x]       2. -> onCreate() 'de initialize et.
-                - [x]       3. -> Initialize edilenleri istediğin yerde çağır.
+    -  [x] Classın içinde TextView Global olarak tanımla.
+    - [x]  onCreate() metodu içinde ise findViewById ile onları al ve init et.
+        -   [x] Yani  1) -> Global Olarak Tanımla
+        -   [x]      2)  -> onCreate() 'de initialize et.
+        -   [x]       3) -> Initialize edilenleri istediğin yerde çağır.
+    
+
+## 4. Time Geriye Sayma ( CountDownTimer )
+- [x] Açılır açılmaz, geriye saysın
+    -  [x] new CountDownTimer( kaçtan_Başlasın ms, SaymaAralığı ms)
+        -   [x]  onTick()  -> Başlangıç time
+        -   [x]  onFinish()  -> Bitiş time
+
+
+## 5. Kenny'i hareket ettir.
+- [x] 9 tane ImageView Global olarak tanımla.
+- [x] onCreate() metounda 9 tanesini initialize et. findViewById ile
+- [x]  ImageView[] arrayini oluştur ve 9 tane initialize olmuş elamanı at.
+  -    [x]  Loopa sokup, diğer kenny'leri görünmez yapacağız. Random olarka 1'er tane açacağız.
+
+- [x]  hideImages() adlı metod oluştur.Açılır, açılmaz saklanması için onCreate() te başlat.
+   -   [x] hideImages altındaki her bir image'yi visible et.
+
--- app/src/main/java/com/ugurhmz/catchkenny/MainActivity.java
+++ app/src/main/java/com/ugurhmz/catchkenny/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ugurhmz.catchkenny;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     TextView timeText;
     TextView scoreText;
     int scoreNumber;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
 
 
 
@@ -25,15 +37,49 @@ public class MainActivity extends AppCompatActivity {
         timeText = findViewById(R.id.timeText);
         scoreText = findViewById(R.id.scoreText);
         scoreNumber = 0;
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+
+        imageArray = new ImageView[] {imageView1,imageView2, imageView3,  imageView4,imageView5, imageView6, imageView7, imageView8, imageView9};
+
+        hideImages();
+
+        new CountDownTimer(10000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                timeText.setText("Time : "+ l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
 
     }
 
-
+    // Increase Score
     public void increaseScore(View view) {
         scoreNumber++;
 
         scoreText.setText("Score : "+scoreNumber);
     }
 
+
+    // Hide Images
+    public void hideImages(){
+        for(ImageView image : imageArray){
+            image.setVisibility(View.INVISIBLE);
+        }
+    }
 
 }
